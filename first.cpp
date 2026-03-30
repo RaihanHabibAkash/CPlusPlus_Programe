34c366aad9e2c89e80fdc6065236c54edde62636
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-30 11:31:35 +0600

    First C++ programe

--- first.cpp
+++ first.cpp
@@ -1,6 +1,16 @@
+/*
+Header file to use input output. iostream => InputOutput Stream
+*/ 
 #include <iostream>
 
 int main() {
     std :: cout << "Hlow bro";
     return 0;
 }
+
+/*
+std -> namespace(container of function/object), standerd.
+cout -> c Output, command for output.
+:: -> scope resolution operator, it says too look inside std for count.
+<< -> Insertion operator, It send/push the data to ouput, {(<)Bahire dike jaitase (mone rakhar jnn)} 
+*/
