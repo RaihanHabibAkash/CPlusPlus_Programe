45d7b490fcd0660ab86cc5301e5bb129d20d1542
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-30 16:05:57 +0600

    Ternary Operator in C++

---
+++ Conditions/ternaryOperator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 11;
+
+    x % 2 == 0 ? cout << "Even\n" : cout << "Odd\n";
+
+    return 0;
+}
+
+/* 
+Ternary Operator: Short cut of if-else
+(condition) ? True : False;
+*/
