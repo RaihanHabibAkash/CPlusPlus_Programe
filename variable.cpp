d79f989f195181ca319c549c41f7fe397c87ee0b
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-30 11:42:17 +0600

    using variable in C++

---
+++ variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int x = 200;
+    char c = 'A';
+    double d = 56.3456;
+
+    std :: cout << x << " " << c << " " << d << std :: endl;
+    std :: cout << "My fav number is " << x << std :: endl;
+
+    return 0;
+}
+
+/*
+std :: endl -> To get new line
+*/
