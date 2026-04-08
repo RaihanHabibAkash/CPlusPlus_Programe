29d4533703db1ba682caff05cc5ebb676f7d8788
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-08 17:20:23 +0600

    We can directly compare 2 string in C++

---
+++ String/compareString.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1 = "Akash";
+    string s2 = "Akash";
+
+    if(s1 == s2) cout << "Same" << endl;
+    else cout << "Not same" << endl;
+
+    return 0;
+}
