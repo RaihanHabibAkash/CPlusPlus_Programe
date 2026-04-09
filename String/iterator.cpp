83b7b7e4f3a0072be590158f200c83aaed9fbe22
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-09 14:36:06 +0600

    Iterators of String in C++

--- String/iterator.cpp
+++ String/iterator.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main() {
-    string s = "Hello";
-    cout << *(s.begin()) << " " << *(s.end() - 1) << endl;
-
-    cout << "Iterator" << endl;
-    for(string::iterator it = s.begin(); it < s.end(); it++) {
-        cout << *it << endl;
+    string s = "Raihan Habib";
+    for(string::iterator it = s.end() - 1; it >= s.begin(); it--) {
+        cout << *it << " ";
     }
+    cout << endl;
+
+    for(auto it = s.begin(); it < s.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
 
     return 0;
 }
+// C++ 11 or above detects the iterator type by using auto
