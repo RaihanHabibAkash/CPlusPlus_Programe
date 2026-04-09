a7d1da7de372d62f8fb2c87f84db52260f4b2175
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-09 17:41:14 +0600

    String Constructor in C++

---
+++ String/stringConstructor.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // 1 -> Normal input
+    // string s("Akash");
+
+    // 2 -> Resize to 3
+    // string s("Akash", 3);
+
+    // 3 -> s ar 2 ta 1st 2ta word bad dia baki (t) te rakhse
+    // string s = "Akash";
+    // string t(s, 2);
+
+    // 4 -> 5 ghor projoto (a) rakhe
+    string s(5, 'a');
+
+    cout << s << endl;
+
+    return 0;
+}
