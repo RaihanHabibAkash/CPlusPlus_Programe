d507b634dc670a71d0010ef57458f42d1048ce1b
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-09 15:48:38 +0600

    String Stream class in C++

--- String/stringStream.cpp
+++ String/stringStream.cpp
@@ -1,50 +1,52 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-//     string s;
-//     // Taking full line input
-//     getline(cin, s);
-//     // Output of the full string
-//     cout << s << endl;
-    
-//     // Declaring stringstream
-//     stringstream ss(s);
-
-//     // For string stram word
-//     string word;
-    
-//     // Getting the 1st word of the string stream
-//     ss >> word;
-//     cout << word << endl;
-
-//     // Getting the 2nd word from string stream
-//     ss >> word;
-//     cout << word << endl;
-
-
-//     return 0;
-// }
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     string s;
+    // Taking full line input
     getline(cin, s);
+    // Output of the full string
     cout << s << endl;
-
-    // Declearing string stream
+    
+    // Declaring stringstream
     stringstream ss(s);
-    string word;
-    int cnt = 0;
+    // stringstream ss;
+    // ss << s;
 
-    while(ss >> word) {
-        cout << word << endl;
-        ++cnt;
-    }
-    cout << cnt << endl;
+    // For string stram word
+    string word;
+    
+    // Getting the 1st word of the string stream
+    ss >> word;
+    cout << word << endl;
+
+    // Getting the 2nd word from string stream
+    ss >> word;
+    cout << word << endl;
+
 
     return 0;
 }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     string s;
+//     getline(cin, s);
+//     cout << s << endl;
+
+//     // Declearing string stream
+//     stringstream ss(s);
+//     string word;
+//     int cnt = 0;
+
+//     while(ss >> word) {
+//         cout << word << endl;
+//         ++cnt;
+//     }
+//     cout << cnt << endl;
+
+//     return 0;
+// }
