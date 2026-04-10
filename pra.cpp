bc194a40d9dce12ee13e2aa39145517240ccccfc
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-10 12:31:42 +0600

    Love char problme solve for Codeforces using C++

--- pra.cpp
+++ pra.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 
 int main() {
-    int arr[10];
+    string s1 = "ak";
+    string s2 = "bh";
+    string s;
 
-    for(int i = 0; i < 10; i++) {
-      cin >> arr[i];
-    }
+    
+        
+        
+        for(auto it = s1.begin(); it < s1.end(); it++) {
+            
+            char word1 = s1[0];
+            s1.erase(0, 1);
+            s.push_back(word1);
 
-    reverse(arr, arr + 10);
-
-    for(int x : arr) {
-      cout << x << " ";
-    }
-    cout << endl;
+            char word2 = s2[0];
+            s2.erase(0, 1);
+            s.push_back(word2);
+        }
+        cout << s;
+        
 
     return 0;
 }
