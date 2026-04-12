28bc1096c3a7674f55c4c184071a8cfbe837e6f4
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-12 11:55:46 +0600

    find() & substr() in C++

--- String/find&substrFunction.cpp
+++ String/find&substrFunction.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 int main() {
     string st = "RaihanHabibAkash";
+    // find() return index of the the find char or string::npos.
     if(st.find("Habib") != string::npos)
         cout << "Paisi" << endl;
     else cout << "Pai nai" << endl;
+
+    // substr(index, 5). index number theke 5 ta value pete chai.
+    cout << st.substr(6, 5) << endl;
     
     return 0;
 }
