d7a7a40ecea9690935b224a8ac35faa145db1d8d
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-20 19:19:53 +0600

    Practiace

--- pra.cpp
+++ pra.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main() {
-    string st = "AkashBro";
-    if(st.find("Akashh") != string::npos) cout << "Paisi" << endl;
-    else cout << "Pai nai" << endl;
+    vector<int> v = {1,2,3,4,2,5,2};
+    for(int x : v) cout << x << " ";
+    cout << endl;
+    
+    vector<int>::iterator it = find(v.begin(), v.end(), 100);
+
+    if(it == v.end()) cout << "Pai nai" << endl;
+    else cout << "Paisi" << endl;
+
+    cout << v.back() << endl;
 
     return 0;
 }
