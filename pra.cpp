eebff6e65ccceee9f093328c8cf1ece816581493
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-22 11:00:18 +0600

    Practice

--- pra.cpp
+++ pra.cpp
@@ -1,76 +1,20 @@
-/*
-L. New Array
-time limit per test1 second
-memory limit per test256 megabytes
-Given two arrays A
- and B
- of size N
-. Print a new array C
- that holds the concatenation of array B
- followed by array A
-.
-
-Note: Solve this problem using function.
-
-Input
-First line will contain a number N
- (1≤N≤103)
-.
-
-Second line will contain N
- numbers (1≤Ai≤105)
- array A
- elements.
-
-Third line will contain N
- numbers (1≤Bi≤105)
- array B
- elements.
-
-Output
-Print array C
- elements separated by space.
-
-Example
-InputCopy
-2
-1 2
-3 4
-OutputCopy
-3 4 1 2 
-*/
-// https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/L
-
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> func(vector<int> &v2, vector<int> &v1) {
-    // Inserting value
-    for(auto it = v2.begin(); it < v2.end(); it++)
-        cin >> *it;
-    for(auto it = v1.begin(); it < v1.end(); it++)
-        cin >> *it;
-
-    // Concating the 2 vector
-    v1.insert(v1.end(), v2.begin(), v2.end());
-
-    // Return vector
-    return v1;
-}
-
 int main() {
-    // Taking input
-    int len; cin >> len;
+    int len, times; cin >> len >> times;
+    vector<int> v(len + 1);
 
-    // Declearing vector
-    vector<int> v2(len);
-    vector<int>v1(len);
+    for(auto it = v.begin()+1; it < v.end(); it++)
+        cin >> *it;
 
-    vector<int> v = func(v2, v1);
+    while(times--) {
+        int l, r; cin >> l >> r;
+        long long sum = 0;
+        for(int i = l; i <= r; i++) sum += v[i];
 
-    // Printing
-    for(int x : v) cout << x << " ";
-    cout << endl;
+        cout << sum << endl;
+    }
 
     return 0;
 }
